d82a0c15137ff4221016771d40d28d40c5a7c6c2
Nora Group <info@noragroup.in>
2026-03-25 18:18:33 +0530

    removed env from tracking

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:3000",
-  process.env.FRONTEND_URL || "https://shopcartshopp.netlify.app"
+  "https://shopcartshopp.netlify.app"
 ];
 
 app.use(cors({
